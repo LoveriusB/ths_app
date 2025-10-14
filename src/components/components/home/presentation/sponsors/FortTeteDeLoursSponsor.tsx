2321f1f7fbc3e7f229e8c3f3e593e734aa5ecc1d
Bruno Loverius <loverius.bruno@gmail.com>
2025-10-14 18:25:56 +0200

    Updated open graphs

--- src/components/components/home/presentation/sponsors/FortTeteDeLoursSponsor.tsx
+++ src/components/components/home/presentation/sponsors/FortTeteDeLoursSponsor.tsx
@@ -33,9 +33,9 @@ export const FortTeteDeLoursSponsor = () => {
               "https://www.facebook.com/p/La-T%C3%AAte-de-lOURS-B%C3%A4renkopf-100057069415706/",
           },
           coverUrl:
-            "https://scontent-bru2-1.xx.fbcdn.net/v/t39.30808-6/481683777_1144127570832879_6843564212942564532_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=YL5fHCN5B-sQ7kNvwEmTNhh&_nc_oc=AdmTHGvmPrTqFNBlYaz8qUUH3tTET9RqvfYjTD1CY2kpvePWwe6AlDbpYlfnRp2ZLdQ&_nc_zt=23&_nc_ht=scontent-bru2-1.xx&_nc_gid=tVgOxMnP6ZIrOrc_hpA-Zw&oh=00_AfeHHmYg8I6oQr16w8Oda1J_oKIWnyY4eDYLfOJQNFoT9Q&oe=68ED87A1",
+            "https://thspublic.s3.eu-west-3.amazonaws.com/barenkopf_background.jpg",
           logoUrl:
-            "https://scontent-bru2-1.xx.fbcdn.net/v/t39.30808-6/358715269_774700704442236_3011480573256548122_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=5T_DaoImXkkQ7kNvwFrhrbh&_nc_oc=AdleT6a2IZnwvkQRJs6LoKZkssQc1DDUy6bgPBkYuorPixgZynVOQ9Wq_TfIid9-aEs&_nc_zt=23&_nc_ht=scontent-bru2-1.xx&_nc_gid=KiYZNdrvik3SeaH6dFbkmw&oh=00_AffvM-ON3GeGThD11nabEdO5dgc9z6gKhU-WM3lfVFaCTw&oe=68ED8BD6",
+            "https://thspublic.s3.eu-west-3.amazonaws.com/barenkopf_logo.jpg",
           locationLabel: "Vosges, France",
           tagline: "Sponsor officiel de l'OP",
         }}
