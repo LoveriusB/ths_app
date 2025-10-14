1de82b8827638fb15b27a947d9ce9caf8073378a
Bruno Loverius <loverius.bruno@gmail.com>
2025-10-14 03:00:00 +0200

    Updated small part of the code

--- src/contexts/AmplifyClient/AmplifyClient.tsx
+++ src/contexts/AmplifyClient/AmplifyClient.tsx
@@ -1,10 +1,11 @@
 // libs/shared-react-resources/src/amplify/context.tsx
-import { createContext } from 'react';
-import { type AmplifyClient } from './AmplifyClientSingleton';
-import amplifyConfig from '../../../amplify_outputs.json';
-import { Amplify } from 'aws-amplify';
-import { generateClient } from 'aws-amplify/api';
-import { Schema } from '../../../amplify/backend';
+import { createContext } from "react";
+import amplifyConfig from "../../../amplify_outputs.json";
+import { Amplify } from "aws-amplify";
+import { Client, generateClient } from "aws-amplify/api";
+import { Schema } from "../../../amplify/backend";
+
+export type AmplifyClient = Client<Schema>;
 
 export const AmplifyClientContext = createContext<AmplifyClient | null>(null);
 
@@ -13,10 +14,11 @@ export const AmplifyClientProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
+  console.log(JSON.stringify({ amplifyConfig }));
   // amorce au montage du provider
   Amplify.configure(amplifyConfig);
   const client = generateClient<Schema>({
-    authMode: 'userPool',
+    authMode: "userPool",
   });
   return (
     <AmplifyClientContext.Provider value={client}>
