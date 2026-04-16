8549e6497c6680ae9edb9746c0ffb972a46a0fb0
Bruno Loverius <loverius.bruno@gmail.com>
2026-04-16 10:09:13 +0800

    Closed registrations

--- src/components/components/home/presentation/Home/ButtonRow.tsx
+++ src/components/components/home/presentation/Home/ButtonRow.tsx
@@ -1,15 +1,21 @@
 import { Button, Grid, useMediaQuery, useTheme } from "@mui/material";
-import { useRegistrationContext } from "../../../../../hooks/useRegistrationContext";
+// import { useRegistrationContext } from "../../../../../hooks/useRegistrationContext";
 
 interface HomeButtonRowProps {
   setRegisterOpen: (open: boolean) => void;
   setSocialNetworkOpen: (open: boolean) => void;
 }
 
-export const HomeButtonRow: React.FC<HomeButtonRowProps> = ({ setRegisterOpen, setSocialNetworkOpen }) => {
+export const HomeButtonRow: React.FC<HomeButtonRowProps> = ({
+  setRegisterOpen,
+  setSocialNetworkOpen,
+}) => {
   const theme = useTheme();
   const isXs = useMediaQuery(theme.breakpoints.only("xs"));
-  const { registration, getAmountOfOrgaRegistration } = useRegistrationContext();
+  // const {
+  //   registration,
+  //   getAmountOfOrgaRegistration,
+  // } = useRegistrationContext();
 
   return (
     <Grid container size={{ xs: 11.5 }} spacing={2}>
@@ -19,9 +25,9 @@ export const HomeButtonRow: React.FC<HomeButtonRowProps> = ({ setRegisterOpen, s
           variant="contained"
           size="large"
           fullWidth={isXs}
-          disabled={registration.length - getAmountOfOrgaRegistration() >= 150}
+          disabled
         >
-          Inscriptions : {registration.length - getAmountOfOrgaRegistration()} / 100
+          Inscriptions fermées
         </Button>
       </Grid>
       <Grid size={{ xs: 6 }} height={"100%"} container alignContent={"center"}>
